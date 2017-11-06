4a5c51eb7e8881f980502e9637a1fe6313825d32
Andrei Duhnea <andrei.duhnea@eaudeweb.ro>
2017-11-06 13:35:46 +0200

    Add Instrument & Obligation to admin

--- reportek/core/admin.py
+++ reportek/core/admin.py
@@ -4,10 +4,13 @@ from .models import (
     Country,
     Client,
     Issue,
+    Instrument,
+    Obligation,
 )
 
 # Register your models here.
 admin.site.register(Country)
 admin.site.register(Client)
 admin.site.register(Issue)
-
+admin.site.register(Instrument)
+admin.site.register(Obligation)
