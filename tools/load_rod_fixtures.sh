e4b9827458294165937d90946505c826bfca5242
Andrei Duhnea <andrei.duhnea@eaudeweb.ro>
2017-11-13 11:19:40 +0200

    Script to load all ROD fixtures

---
+++ tools/load_rod_fixtures.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+python manage.py loaddata data/fixtures/countries.yaml
+python manage.py loaddata data/fixtures/instruments.yaml
+python manage.py loaddata data/fixtures/issues.yaml
+python manage.py loaddata data/fixtures/clients.yaml
+python manage.py loaddata data/fixtures/obligations.yaml
