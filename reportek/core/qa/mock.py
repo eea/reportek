90177203bbe168442c3a26b739630baa1bd2ec31
Andrei Duhnea <andrei.duhnea@eaudeweb.ro>
2017-11-21 16:11:25 +0200

    [refs #89675] Parametrize mock QA connector

--- reportek/core/qa/mock.py
+++ reportek/core/qa/mock.py
@@ -14,8 +14,10 @@ class QAConnectionMock:
     """
     Simulates calling QA and receving a response
     """
-    def __init__(self):
+    def __init__(self, success_chance=.5, min_delay=0):
         self.requests = {}  # Maps request ids to response handlers
+        self.success_chance = success_chance  # Defaults to a 50% chance
+        self.min_delay = min_delay
 
     def send(self, envelope, response_handler):
         request_id = uuid4()
@@ -23,7 +25,7 @@ class QAConnectionMock:
         info(f'[QA RPC] Sending envelope "{envelope.name}" to QA [id={request_id}] ...')
         # Fake a variable time response
         t = threading.Timer(
-            interval=(1 + random() * 1.5),
+            interval=(self.min_delay + random() * 1.5),
             function=self.get_response,
             args=(request_id,)
         )
@@ -34,10 +36,9 @@ class QAConnectionMock:
         """
         Mocks a response with 50% chances of success.
         """
-        # time.sleep((1 + random() * 1.5))
         response = {
             'id': request_id,
-            'valid': random() >= .5
+            'valid': random() >= 1 - self.success_chance
         }
         info(f'[QA RPC] Received QA response for request id "{request_id}": '
              f'{"VALID" if response["valid"] else "INVALID"}')
