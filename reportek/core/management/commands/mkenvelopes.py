8ac14e1364846dc25b603a3c590265bbbf16874c
Ionuț Ciocîrlan <ionut.ciocirlan@eaudeweb.ro>
2017-11-23 16:06:02 +0200

    [refs #89677] "fix" mkenvelopes management command

--- reportek/core/management/commands/mkenvelopes.py
+++ reportek/core/management/commands/mkenvelopes.py
@@ -2,6 +2,7 @@ import os
 import random
 from copy import deepcopy
 from datetime import date, timedelta
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand, CommandError
 from django.core.files.base import ContentFile
 from django.contrib.postgres.aggregates import ArrayAgg
@@ -9,7 +10,7 @@ from edw.djutils.management import ProgressMixin
 
 from reportek.core.models import (
     Envelope, EnvelopeFile,
-    Obligation,
+    Obligation, ReportingPeriod,
 )
 
 
@@ -20,7 +21,10 @@ DEFAULT_MAX_SIZE = 1024 * 1024
 
 
 class Command(ProgressMixin, BaseCommand):
-    help = "Generate envelopes with files attached."
+    help = (
+        "Generate envelopes with files attached."
+        " WARNING: will make a mess of the database."
+    )
 
     PROGRESS_PREFIX = "Generating envelopes: "
 
@@ -48,7 +52,7 @@ class Command(ProgressMixin, BaseCommand):
                                 "minimum file size in bytes"
                                 " (default %s)" % DEFAULT_MIN_SIZE
                             ))
-        
+
         parser.add_argument('--max-file-size', type=int,
                             default=DEFAULT_MAX_SIZE,
                             dest='max_size', metavar='BYTES',
@@ -57,15 +61,15 @@ class Command(ProgressMixin, BaseCommand):
                                 " (default %s)" % DEFAULT_MAX_SIZE
                             ))
 
-    def handle(self, 
+    def handle(self,
                envelopes=DEFAULT_ENVELOPES, files=DEFAULT_FILES,
                min_size=DEFAULT_MIN_SIZE, max_size=DEFAULT_MAX_SIZE,
                **options):
         ogroups = {
-            row['group']: list(set(row['countries']))
+            row['group']: {'countries': list(set(row['countries']))}
             for row in (
                 Obligation.objects
-                .filter(group__isnull=False)
+                .filter(group__workflow_class__isnull=False)
                 .values('group')
                 .annotate(countries=ArrayAgg('delivery_countries'))
             )
@@ -78,27 +82,37 @@ class Command(ProgressMixin, BaseCommand):
                 start = (end - timedelta(3 * 29)).replace(day=1)
                 yield [start, end]
                 end = start - timedelta(days=1)
-        
+
         ranges = mkranges()
-        period = None
+        period, prange = None, None
         groups = {}
 
         for i in range(envelopes):
-            # pick each time a different combination of ogroup / country
-            # when done, then start anew with another period
+            # pick each time a different combination of ogroup / country.
+            # when done start anew with another period.
             if not groups:
                 groups = deepcopy(ogroups)
-                period = next(ranges)
+                prange = next(ranges)
+
+                for group in groups:
+                    try:
+                        period = ReportingPeriod.objects.current().get(
+                            obligation_group_id=group)
+                    except ObjectDoesNotExist:
+                        period = ReportingPeriod.objects.create(
+                            obligation_group_id=group,
+                            period=prange)
+
+                    groups[group]['period'] = period
 
             group = random.choice(list(groups.keys()))
-            countries = groups[group]
+            countries = groups[group]['countries']
             cidx = random.randrange(len(countries))
             country = countries.pop(cidx)
 
             e = Envelope.objects.create(
                 name="Envelope %s" % i,
                 slug="e-%s" %i,
-                reporting_period=period,
                 country_id=country,
                 obligation_group_id=group,
             )
@@ -115,6 +129,7 @@ class Command(ProgressMixin, BaseCommand):
                 )
 
             if not countries:
+                groups[group]['period'].close()
                 del groups[group]
 
             self.progress(i + 1, envelopes)
