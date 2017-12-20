f7e255140163fa7f099f4256fee27f686ccfebde
Andrei Duhnea <andrei@duhnea.net>
2017-12-20 13:50:55 +0200

    Dockerize celery worker & bear

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if [ -z "$POSTGRES_ADDR" ]; then
-  export POSTGRES_ADDR="postgres"
+if [ -z "$POSTGRES_HOST" ]; then
+  export POSTGRES_HOST="postgres"
 fi
 
-while ! nc -z $POSTGRES_ADDR 5432; do
-  echo "Waiting for Postgres server at '$POSTGRES_ADDR' to accept connections on port 5432..."
+while ! nc -z ${POSTGRES_HOST} 5432; do
+  echo "Waiting for Postgres server at '$POSTGRES_HOST' to accept connections on port 5432..."
   sleep 1s
 done
 
---
+++ run_celery_worker.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ -z "$RABBITMQ_HOST" ]; then
+  export RABBITMQ_HOST="rabbitmq"
+fi
+
+while ! nc -z ${RABBITMQ_HOST} 5672; do
+  echo "Waiting for RabbitMQ server at '$RABBITMQ_HOST' to accept connections on port 5672..."
+  sleep 1s
+done
+
+if [ -z "$POSTGRES_HOST" ]; then
+  export POSTGRES_HOST="postgres"
+fi
+
+while ! nc -z ${POSTGRES_HOST} 5432; do
+  echo "Waiting for Postgres server at '$POSTGRES_ADDR' to accept connections on port 5432..."
+  sleep 1s
+done
+
+exec celery -A reportek.site worker -l info -P gevent
