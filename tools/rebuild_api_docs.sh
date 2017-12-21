e95186aef98c1b2e5815dd28b24b5b6a9ac38cf8
Andrei Duhnea <andrei@duhnea.net>
2017-12-21 12:19:49 +0200

    Update apidocs exclude list

--- tools/rebuild_api_docs.sh
+++ tools/rebuild_api_docs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 rm -f docs/api/*
-sphinx-apidoc -o docs/api reportek reportek/core/migrations
+sphinx-apidoc -o docs/api reportek reportek/core/migrations reportek/tests reportek/core/management/commands reportek/site/localsettings.py
