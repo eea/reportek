f4c0e5dfb652ff56efbbc01d4eb22a1184b66852
Andrei Duhnea <andrei@duhnea.net>
2018-01-08 12:57:41 +0200

    Add CORS middleware

--- reportek/site/settings.py
+++ reportek/site/settings.py
@@ -84,6 +84,7 @@ INSTALLED_APPS = [
     'django_celery_beat',
     'django_extensions',
     'django_object_actions',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -95,12 +96,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'reportek.site.urls'
 
