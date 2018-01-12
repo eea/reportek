effb2a41e3927b7eb6d77477692a96182816ba55
Alexandru Medesan <alex.medesan@gmail.com>
2018-01-12 13:47:44 +0200

    fixed bug with importing scss files

--- frontend/config/utils.js
+++ frontend/config/utils.js
@@ -60,7 +60,7 @@ exports.cssLoaders = function (options) {
     postcss: generateLoaders(),
     less: generateLoaders('less'),
     sass: generateLoaders('sass', { indentedSyntax: true }),
-    scss: generateLoaders('sass'),
+    // scss: generateLoaders('sass'), // commented because the scss is already defined in webpack.base.conf.js and it conflicts
     stylus: generateLoaders('stylus'),
     styl: generateLoaders('stylus')
   }
