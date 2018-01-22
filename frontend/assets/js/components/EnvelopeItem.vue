00993710b68506a8f937f9d79b3210891c68a309
Alexandru Medesan <alex.medesan@gmail.com>
2018-01-22 16:30:20 +0200

    files upload sequentially with update envelopes and uploading files list after upload finished through polling for files length

--- frontend/assets/js/components/EnvelopeItem.vue
+++ frontend/assets/js/components/EnvelopeItem.vue
@@ -107,7 +107,14 @@
 
 <script>
 import tus from 'tus-js-client';
-import { fetchEnvelope, fetchEnvelopeToken, fetchEnvelopeHistory, fetchEnvelopeFeedback, fetchEnvelopeFilesQAScripts, runEnvelopeFilesQAScript } from '../api';
+import { fetchEnvelope,
+          fetchEnvelopeToken,
+          fetchEnvelopeHistory,
+          fetchEnvelopeFeedback,
+          fetchEnvelopeFilesQAScripts,
+          runEnvelopeFilesQAScript,
+          fetchEnvelopeFiles
+          } from '../api';
 
 export default {
   name: 'EnvelopeItem',
@@ -138,50 +145,71 @@ export default {
       for (const file of newfiles) {
         this.files.push({file: file, percentage: 0});
       }
-
-      console.log(this.files);
     },
 
     uploadAllFiles(e) {
       e.preventDefault();
-      fetchEnvelopeToken(this.$route.params.envelope_id)
-        .then((response) => {
-          const token = response.data.token;
-          for (let index = 0; index < this.files.length; index++) {
-            const file = this.files[index];
-            this.uploadFile(file.file, token, index);
-          }
-        });
+
+      // for each file create a function that returns a promise
+      const funcs = this.files.map((file, index) => () => this.uploadFile(file, index))
+
+      // reduce the array or functions that return promises, in a chain
+      const promiseSerial = funcs =>
+        funcs.reduce((promise, func) =>
+          promise.then(result =>
+            func().then(result => {this.updateFilesList();})
+          ),
+          Promise.resolve([])
+        );
+
+      // execute Promises in serial, clear files at the end of all promisees
+      promiseSerial(funcs)
+        .then(() => {this.files = [];})
+        .catch(console.error.bind(console));
     },
 
-    uploadFile(file, token, index) {
+    uploadFile(file, index) {
       // Create a new tus upload
       var self = this;
-      const upload = new tus.Upload(file,
-      {
-        endpoint: 'http://localhost:1080/files/',
-        metadata: {
-          token,
-          filename: file.name,
-          index: index,
-          fileId: file.id
-        },
-        retryDelays: [0, 1000, 3000, 5000],
-        onError: function onError(error) {
-          console.log('Failed because: ', error);
-        },
-        onProgress: function onProgress(bytesUploaded, bytesTotal) {
-          const index = this.metadata.index;
-          const percentage = self.files[index].percentage = parseInt(((bytesUploaded / bytesTotal) * 100).toFixed(2));
-          console.log(bytesUploaded, bytesTotal, percentage, '%');
-        },
-        onSuccess: function onSuccess() {
-          console.log('Download %s from %s', upload.file.name, upload.url);
-        },
-      });
+      return new Promise((resolve, reject) => {
 
-      // Start the upload
-      upload.start();
+        fetchEnvelopeToken(this.$route.params.envelope_id)
+          .then((response) => {
+            const token = response.data.token;
+
+            const upload = new tus.Upload(file.file,
+              {
+                endpoint: 'http://localhost:1080/files/',
+                metadata: {
+                  token,
+                  filename: file.file.name,
+                  index: index,
+                  fileId: file.file.id
+                },
+                retryDelays: [0, 1000, 3000, 5000],
+                onError: function onError(error) {
+                  console.log('Failed because: ', error);
+                  reject(error);
+                },
+                onProgress: function onProgress(bytesUploaded, bytesTotal) {
+                  const index = this.metadata.index;
+                  const percentage = file.percentage = parseInt(((bytesUploaded / bytesTotal) * 100).toFixed(2));
+                  console.log(bytesUploaded, bytesTotal, percentage, '%');
+                },
+                onSuccess: function onSuccess() {
+                  console.log('Download %s from %s', upload.file.name, upload.url);
+                  resolve(
+                    {
+                      fileName: upload.file.name,
+                      uploadUrl: upload.url
+                    }
+                  );
+                },
+              });
+              // Start the upload
+              upload.start();
+            });
+      });
     },
 
     getEnvelope() {
@@ -229,6 +257,37 @@ export default {
         });
 
     },
+
+    updateFilesList() {
+      return this.pollFiles(() => fetchEnvelopeFiles(this.$route.params.envelope_id), 500);
+    },
+
+    pollFiles(fn, delay) {
+      const self = this;
+      let pollFiles = setTimeout(() => {
+        fn()
+          .then(function (response) {
+            if(self.envelope.files.length === response.data.length) {
+              self.pollFiles(fn, delay);
+            } else {
+              for (let index = 0; index <= response.data.length; index++) {
+                const responseFile = response.data[index];
+                const found = self.envelope.files.find(function(file) {
+                  return file.id === responseFile.id;
+                });
+
+                if(found === undefined) {
+                  self.envelope.files.push(response.data[index]);
+                  self.files.shift();
+                }
+              }
+            }
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }, delay);
+    }
   },
 };
 </script>
