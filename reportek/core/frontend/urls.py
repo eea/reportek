4412733f3fbbf91d0b300d0ff2f31b44d033ed31
Andrei Duhnea <andrei@duhnea.net>
2018-01-23 11:29:55 +0200

    Add envelope metadata endpoint (non-API)

--- reportek/core/frontend/urls.py
+++ reportek/core/frontend/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     url(r'^envelopes/(?P<pk>[0-9]+)/$',
         views.EnvelopeView.as_view(),
         name='envelope'),
-
+    url(r'^envelopes/(?P<pk>[0-9]+)/xml$',
+        views.EnvelopeMetadataView.as_view(),
+        name='envelope-metadata'),
     url(r'^envelopes/(?P<pk>[0-9]+)/(?P<filename>[^/]+)$',
         views.EnvelopeFileDownloadView.as_view(),
         name='envelope-file'),
--- reportek/core/frontend/views.py
+++ reportek/core/frontend/views.py
@@ -1,6 +1,7 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.http.response import Http404
 from django.utils.functional import cached_property
+from django.shortcuts import render
 from edw.djutils import protected
 
 from ..models import (
@@ -13,6 +14,21 @@ class EnvelopeView(protected.views.ProtectedDetailView):
     model = Envelope
     permission_classes = (permissions.IsAuthenticatedOrEnvelopeIsPublic, )
     template_name = 'envelope.html'
+
+
+class EnvelopeMetadataView(protected.views.ProtectedDetailView):
+    model = Envelope
+    permission_classes = (permissions.IsAuthenticatedOrEnvelopeIsPublic, )
+    template_name = 'envelope_xml.jinja2'
+
+    def get(self, request, *args, **kwargs):
+        envelope = self.get_object()
+        return render(
+            request,
+            template_name=self.template_name,
+            context={'envelope': envelope},
+            content_type='application/xml'
+        )
 
 
 class EnvelopeFileDownloadView(protected.views.ProtectedBaseDetailFileView):
