2d9abacdba4c2d4a3bad60757e0979e05adbfc48
Mihai Macaneata <themacaneata@gmail.com>
2018-02-19 18:36:58 +0200

    fixed binary file response

--- frontend/assets/js/api/index.js
+++ frontend/assets/js/api/index.js
@@ -78,15 +78,17 @@ export function fetchEnvelopeFilesConvertScripts(id, fileId) {
 }
 
 export function runEnvelopeFilesConvertScript(id, fileId, scriptId) {
-  return api.post(`envelopes/${id}/files/${fileId}/run_conversion_script/`,
-    {
-      data: {
-        'convert_id': scriptId
-      },
-     responseType: 'arrayBuffer'
-    },
-
-    );
+// Using the post method doesn't work. We have to use a new axios instance
+  return  axios({
+            baseURL: 'http://localhost:8000/api/0.1/',
+            withCredentials: true,
+            method:'post',
+            xsrfCookieName: "csrftoken",
+            xsrfHeaderName: "X-CSRFTOKEN",
+            url:`envelopes/${id}/files/${fileId}/run_conversion_script/`,
+            responseType:'arraybuffer',
+            data: {convert_id: scriptId}
+          })
 }
 
 
