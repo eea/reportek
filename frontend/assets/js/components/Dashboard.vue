1f8cd17c6962382dbf4bc89f185d3e032be14b7d
Mihai Macaneata <themacaneata@gmail.com>
2018-03-06 15:16:04 +0200

    dashboard design wip

--- frontend/assets/js/components/Dashboard.vue
+++ frontend/assets/js/components/Dashboard.vue
@@ -1,44 +1,66 @@
 <template>
   <div>
-    <div>
+    <div class="dashboard">
+      <b-row class="dashboard-title">
+        <h1>Envelopes</h1>
+      </b-row>
+
+
       <wip
         v-on:envelopesWipLoaded="handleEnvelopesWipLoaded($event)"
         context="dashboardComponent"
         :envelopesCount="envelopesCount"
       >
       </wip>
-      <router-link
-        class="nav-link"
-        v-if="showMoreWip"
-        :to="{ name: 'EnvelopesWIP'}"
-      >
-        Show {{actualLength.envelopesWipLength - envelopesCount}} more envelopes
-      </router-link>
+      <div v-if="showMoreArchive" class="show-more">
+        <span>Showing {{envelopesCount}} out of {{actualLength.envelopesWipLength}} envelopes.</span>
+        <router-link
+          class="nav-link"
+          style="display: inline-block"
+          :to="{ name: 'EnvelopesWIP'}"
+        >
+        Show more
+        </router-link>
+      </div>
+
+
       <envelopeArchive
         v-on:archiveLoaded="handleArchiveLoaded($event)"
         :archiveCount="archiveCount"
         context="dashboardComponent"
       >
       </envelopeArchive>
-      <router-link
-        class="nav-link"
-        :to="{ name: 'EnvelopesArchive', params: { spec: 1 } }"
-      >
-        Show {{actualLength.archiveLength - archiveCount}} more envelopes
+      <div v-if="showMoreArchive" class="show-more">
+        <span >Showing {{archiveCount}} out of {{actualLength.archiveLength}} envelopes.</span>
+        <router-link
+          class="nav-link"
+          style="display: inline-block"
+          :to="{ name: 'EnvelopesArchive', params: { spec: 1 } }"
+        >
+          Show more
+        </router-link>
+      </div>
 
-      </router-link>
+
       <obligations-pending
         context="dashboardComponent"
         v-on:obligationsLoaded="handleObligationsLoaded($event)"
         :obligationsCount="obligationsCount"
       >
       </obligations-pending>
-      <router-link
-        class="nav-link"
-        :to="{ name: 'ObligationsPending', params: { reporterId: `${$route.params.reporterId}` } }"
-      >
-        Show {{actualLength.obligationsLength - obligationsCount}} more obligations
-      </router-link>
+
+      <div v-if="showMoreObligations" class="show-more">
+        <span v-if="showMoreObligations">Showing {{obligationsCount}} out of {{actualLength.obligationsLength}} obligations.</span>
+        <router-link
+          class="nav-link"
+          style="display: inline-block"
+          :to="{ name: 'ObligationsPending', params: { reporterId: `${$route.params.reporterId}` } }"
+        >
+          Show more
+        </router-link>
+      </div>
+
+
     </div>
 
   </div>
@@ -94,6 +116,45 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+.dashboard-component {
+  h1 {
+    font-size: 1.4rem;
+  }
+  .envelope-listing-header {
+    margin-top: 1rem!important;
+  }
 
+  .badge-pill {
+    line-height: 1.4;
+    height: 41px!important;
+    font-size: 1.3rem;
+  }
+  .envelope-name .router-link{
+    font-size: 1.4rem!important;
+    font-weight: 400!important;
+  }
+}
+
+.dashboard {
+  .nav-link {
+    padding: 0;
+  }
+
+ .dashboard-title {
+  margin-top: 2rem;
+  border-bottom: 1px solid #eee;
+ }
+
+  .show-more {
+    display: block;
+    background: #f7f7f7;
+    padding: .5rem;
+    font-size: .8rem;
+    margin-top: -1rem;
+    margin-left: .5rem;
+    margin-right: .5rem;
+    padding-left: 2rem;
+  }
+}
 </style>
