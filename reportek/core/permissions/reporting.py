ce22c2af34c78fc8067c53ccd3ca6c9f4dcaeb1d
Andrei Duhnea <andrei@duhnea.net>
2018-04-03 15:01:23 +0300

    Use skip_for_superuser in envelope permissions checks

--- reportek/core/permissions/reporting.py
+++ reportek/core/permissions/reporting.py
@@ -1,21 +1,12 @@
 import logging
 from django.db.models import ObjectDoesNotExist
-from rest_framework.permissions import (
-    SAFE_METHODS,
-)
+from rest_framework.permissions import SAFE_METHODS
 
-from ..models import (
-    ObligationSpec,
-    Reporter,
-    Envelope,
-)
+from ..models import ObligationSpec, Reporter, Envelope
 
 from .base import EffectiveObjectPermissions
 
-from .utils import (
-    get_groups_obj_perms,
-    debug_call,
-)
+from .utils import get_groups_obj_perms, debug_call, skip_for_superuser
 
 
 log = logging.getLogger('reportek.auth')
@@ -26,9 +17,7 @@ error = log.error
 
 
 __all__ = [
-    'EnvelopePermissions',
-    'EnvelopeFilePermissions',
-    'EnvelopeOriginalFilePermissions',
+    'EnvelopePermissions', 'EnvelopeFilePermissions', 'EnvelopeOriginalFilePermissions'
 ]
 
 
@@ -38,20 +27,26 @@ def has_reporter_permissions(groups, reporter, obligation):
     debug(f'Perms on reporter "{reporter}": {reporter_perms}')
     debug(f'Perms on obligation "{obligation}": {obligation_perms}')
     return (
-        'report_for_reporter' in reporter_perms and
-        'report_on_obligation' in obligation_perms
+        'report_for_reporter' in reporter_perms
+        and 'report_on_obligation' in obligation_perms
     )
 
 
 class EnvelopePermissions(EffectiveObjectPermissions):
 
     @debug_call
+    @skip_for_superuser
     def has_permission(self, request, view):
+        if request.user.is_superuser:
+            return True
+
         perms = super().has_permission(request, view)
         if not perms:
             return False
+
         if request.method in SAFE_METHODS:
             return True
+
         elif request.method == 'POST':
             # Creating or other POSTS on an envelope requires permission to report on
             # the obligation on behalf of the reporter.
@@ -64,31 +59,54 @@ class EnvelopePermissions(EffectiveObjectPermissions):
                     obligation_spec = ObligationSpec.objects.get(pk=obligation_spec_id)
                 except ObjectDoesNotExist:
                     return False
-                return has_reporter_permissions(groups, reporter, obligation_spec.obligation)
+
+                return has_reporter_permissions(
+                    groups, reporter, obligation_spec.obligation
+                )
+
             else:
                 # Non-create POST requests, e.g. `transition`
                 try:
-                    envelope = Envelope.objects.get(pk=request.resolver_match.kwargs.get('pk'))
+                    envelope = Envelope.objects.get(
+                        pk=request.resolver_match.kwargs.get('pk')
+                    )
                 except Envelope.DoesNotExist:
                     return False
 
-                return has_reporter_permissions(groups, envelope.reporter, envelope.obligation_spec.obligation)
+                return has_reporter_permissions(
+                    groups, envelope.reporter, envelope.obligation_spec.obligation
+                )
 
         # Allow GET detail, PATCH, PUT & DELETE to fall through to `has_object_permissions`
         return True
 
     @debug_call
+    @skip_for_superuser
     def has_object_permission(self, request, view, envelope):
         # We do NOT check object permissions on the Envelope object itself,
         # as there are none currently in use.
+
+        if request.user.is_superuser:
+            return True
+
         if request.method in SAFE_METHODS:
             return True
+
         elif request.method == 'PATCH':
             return request.user == envelope.assigned_to and not envelope.finalized
+
         elif request.method == 'DELETE':
             return request.user == envelope.assigned_to and not envelope.finalized
+
         elif request.method == 'POST' and view.action != 'create':
-            return request.user == envelope.assigned_to and not envelope.finalized
+            if envelope.finalized:
+                return False
+
+            elif view.action == 'assign' and envelope.assigned_to is None:
+                return True
+
+            else:
+                return request.user == envelope.assigned_to and not envelope.finalized
 
         return False
 
@@ -96,12 +114,18 @@ class EnvelopePermissions(EffectiveObjectPermissions):
 class BaseEnvelopeFilePermissions(EffectiveObjectPermissions):
 
     @debug_call
+    @skip_for_superuser
     def has_permission(self, request, view):
         perms = super().has_permission(request, view)
         if not perms:
             return False
+
+        if request.user.is_superuser:
+            return True
+
         if request.method in SAFE_METHODS:
             return True
+
         elif request.method == 'POST' and view.action == 'create':
             envelope_id = request.data.get('envelope_pk')
         else:
@@ -119,21 +143,32 @@ class BaseEnvelopeFilePermissions(EffectiveObjectPermissions):
             return False
 
         groups = request.user.effective_groups
-        return has_reporter_permissions(groups, envelope.reporter, envelope.obligation_spec.obligation)
+        return has_reporter_permissions(
+            groups, envelope.reporter, envelope.obligation_spec.obligation
+        )
 
     @debug_call
+    @skip_for_superuser
     def has_object_permission(self, request, view, envelope_file):
         # We do NOT check object permissions on the EnvelopeFile object itself,
         # as there are none currently in use.
+
+        if request.user.is_superuser:
+            return True
+
         envelope = envelope_file.envelope
         if request.method in SAFE_METHODS:
             return True
+
         elif request.method == 'PATCH':
             return request.user == envelope.assigned_to and not envelope.finalized
+
         elif request.method == 'DELETE':
             return request.user == envelope.assigned_to and not envelope.finalized
+
         elif request.method == 'POST' and view.action != 'create':
             return request.user == envelope.assigned_to and not envelope.finalized
+
         return False
 
 
